e63e5ab2075d52f498e3d88ff69117d999566279
ankeshbanerjee <banerjeeankesh@gmail.com>
2024-05-07 17:39:57 +0530

    updated bottomsheet component

--- src/components/BottomSheetComponent.tsx
+++ src/components/BottomSheetComponent.tsx
@@ -1,30 +1,37 @@
+import {StyleSheet} from 'react-native';
 import React, {useMemo, PropsWithChildren, useContext} from 'react';
 import {BottomSheetModal} from '@gorhom/bottom-sheet';
 import {
   BottomSheetBackdrop,
   BottomSheetBackdropProps,
 } from '@gorhom/bottom-sheet';
+import {BottomSheetModalMethods} from '@gorhom/bottom-sheet/lib/typescript/types';
 import {ThemeContext} from '../contexts/theme_context';
 
 export type BottomSheetRefType = BottomSheetModal;
 
 type Props = {
   snapPoint: number;
+  hideHandle?: boolean;
+  removeTopPadding?: boolean;
   onDismissCallback?: () => void;
+  onChange?: (index: number) => void;
 };
 
 const BottomSheetComponent = React.forwardRef<
-  BottomSheetModal,
+  BottomSheetModalMethods,
   PropsWithChildren<Props>
 >((props, ref) => {
+  const snapPoints = useMemo(() => [`${props.snapPoint}%`], []);
   const {theme} = useContext(ThemeContext);
-  const snapPoints = useMemo(() => [`${props.snapPoint}%`, '90%'], []);
   return (
     <BottomSheetModal
       ref={ref}
       handleIndicatorStyle={{
-        backgroundColor: theme.secondaryColor,
+        height: props.hideHandle ? 0 : 6,
+        backgroundColor: theme.secondaryContainer,
       }}
+      handleStyle={props.removeTopPadding ? {padding: 0} : null}
       backdropComponent={(props: BottomSheetBackdropProps) => (
         <BottomSheetBackdrop
           disappearsOnIndex={-1}
@@ -32,13 +39,18 @@ const BottomSheetComponent = React.forwardRef<
           {...props}
         />
       )}
+      onChange={i => {
+        if (props.onChange !== undefined) {
+          props.onChange(i);
+        }
+      }}
       enablePanDownToClose={true}
       enableDismissOnClose={true}
       index={0}
       snapPoints={snapPoints}
       onDismiss={props.onDismissCallback ? props.onDismissCallback : () => {}}
       backgroundStyle={{
-        backgroundColor: theme.surfaceVariant,
+        backgroundColor: theme.backgroungColor,
       }}>
       {props.children}
     </BottomSheetModal>
@@ -46,3 +58,5 @@ const BottomSheetComponent = React.forwardRef<
 });
 
 export default BottomSheetComponent;
+
+const styles = StyleSheet.create({});
